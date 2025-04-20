4300e7f5a112e5f58713b2b4255f4a2017b5a81c
Wong Kian Yoou <kywong1603@gmail.com>
2025-04-20 20:29:32 +0800

    Part Three Completed

--- TechnicalAssesmentBackendDeveloper/Program.cs
+++ TechnicalAssesmentBackendDeveloper/Program.cs
@@ -16,7 +16,13 @@
         manager.PrintAllItems();
 
         // Part Three: Introduce a Fruit class and use the ItemManager<Fruit> to add a few fruits and print them on the console.
-        // TODO: Implement this part three.
+        ItemManager<Fruit> fruit = new ItemManager<Fruit>();
+
+        fruit.AddItem(new Fruit { name = "Orange" });
+        fruit.AddItem(new Fruit { name = "Mango" });
+        fruit.AddItem(new Fruit { name = "Durian" });
+
+        fruit.PrintAllItems();
 
         // Part Four (Bonus): Implement an interface IItemManager and make ItemManager implement it.
         // TODO: Implement this part four.
@@ -54,7 +60,7 @@ public class ItemManager
 
 public class ItemManager<T>
 {
-    private List<T> items;
+    private List<T> items = new List<T>();
 
     public void AddItem(T item)
     {
@@ -73,4 +79,15 @@ public class ItemManager<T>
     {
         items = [];
     }
+}
+
+public class Fruit
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return "Fruit: " + name;
+    }
+
 }
